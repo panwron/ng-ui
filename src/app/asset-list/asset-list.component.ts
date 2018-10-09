566047748ee0171584cc416f6316f90fc06906d3
Maciej Wrona <panwron@gmail.com>
2018-10-09 23:46:41 +0200

    get category name on select change

--- src/app/asset-list/asset-list.component.ts
+++ src/app/asset-list/asset-list.component.ts
@@ -11,15 +11,15 @@ export class AssetListComponent implements OnInit {
   public assetCategoriesElements = [ 'Asset 1', 'Asset 2', 'Asset 3', 'Asset 4', 'Asset 5'];
   public assetCategories = [
     {
-     name: "category1",
+     name: "category 1",
      asset: [ 'Asset 1 1', 'Asset 2 1', 'Asset 3 1', 'Asset 4 1', 'Asset 5 1']
     },
     {
-      name: "category3",
+      name: "category 2",
       asset: [ 'Asset 1 2', 'Asset 2 2', 'Asset 3 2', 'Asset 4 2', 'Asset 5 2']
      },
      {
-      name: "category2",
+      name: "category 3",
       asset: [ 'Asset 1 3', 'Asset 2 3', 'Asset 3 3', 'Asset 4 3', 'Asset 5 3']
      }
   ];
@@ -34,9 +34,16 @@ export class AssetListComponent implements OnInit {
 
   }
 
+  selectOnChange(e) {
+    console.log(e);
+
+  
+
+  }
+
   ngOnInit() {
 
-    this.activeCategoryItems =  this.getactiveCategory("category1");
+    this.activeCategoryItems =  this.getactiveCategory("category 1");
 
   }
   

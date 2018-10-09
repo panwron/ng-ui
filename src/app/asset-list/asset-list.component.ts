60fcb8448e74b7db9fa942d02349cdf1a7da5e5e
Maciej Wrona <panwron@gmail.com>
2018-10-09 23:28:19 +0200

    filter asets by category

--- src/app/asset-list/asset-list.component.ts
+++ src/app/asset-list/asset-list.component.ts
@@ -11,21 +11,33 @@ export class AssetListComponent implements OnInit {
   public assetCategoriesElements = [ 'Asset 1', 'Asset 2', 'Asset 3', 'Asset 4', 'Asset 5'];
   public assetCategories = [
     {
-     category: "category1",
+     name: "category1",
      asset: [ 'Asset 1 1', 'Asset 2 1', 'Asset 3 1', 'Asset 4 1', 'Asset 5 1']
     },
     {
-      category: "category3",
+      name: "category3",
       asset: [ 'Asset 1 2', 'Asset 2 2', 'Asset 3 2', 'Asset 4 2', 'Asset 5 2']
      },
      {
-      category: "category2",
+      name: "category2",
       asset: [ 'Asset 1 3', 'Asset 2 3', 'Asset 3 3', 'Asset 4 3', 'Asset 5 3']
      }
   ];
+
+  public activeCategoryItems = null;
   constructor() { }
 
+  getactiveCategory(categoryName){
+    
+    const activeCat = this.assetCategories.filter((category) => category.name == categoryName )
+    return activeCat[0].asset
+
+  }
+
   ngOnInit() {
+
+    this.activeCategoryItems =  this.getactiveCategory("category1");
+
   }
   
 }
